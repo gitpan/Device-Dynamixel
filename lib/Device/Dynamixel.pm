2eb2b241c2631a49def4b315db5761574950fb06
Dima Kogan <cpan@dima.secretsauce.net>
2011-03-04 06:57:40 +0000

    Import of DKOGAN/Device-Dynamixel-0.025 from CPAN.
    gitpan-cpan-distribution: Device-Dynamixel
    gitpan-cpan-version:      0.025
    gitpan-cpan-path:         DKOGAN/Device-Dynamixel-0.025.tar.gz
    gitpan-cpan-author:       DKOGAN
    gitpan-cpan-maturity:     released

--- lib/Device/Dynamixel.pm
+++ lib/Device/Dynamixel.pm
@@ -10,13 +10,9 @@ use Const::Fast;
 
 Device::Dynamixel - Simple control of Robotis Dynamixel servo motors
 
-=head1 VERSION
-
-Version 0.024
-
 =cut
 
-our $VERSION = '0.024';
+our $VERSION = '0.025';
 
 
 =head1 SYNOPSIS
@@ -24,12 +20,20 @@ our $VERSION = '0.024';
  use Device::Dynamixel;
 
  open my $pipe, '+<', '/dev/ttyUSB0' or die "Couldn't open pipe for reading and writing";
+ my $motorbus = Device::Dynamixel->new($pipe);
 
- my $dynamixel = Dynamixel->new($pipe, 5);
- while(<>)
- {
-   $dynamixel->moveMotorTo_deg($_);
- }
+ # set motion speed of ALL motors to 200
+ $motorbus->writeMotor($Device::Dynamixel::BROADCAST_ID,
+                       $Device::Dynamixel::addresses{Moving_Speed_L}, [200, 0]);
+
+ # move motor 5 to 10 degrees off-center
+ $motorbus->moveMotorTo_deg(5, 10);
+
+ # read the position of motor 5
+ my $status = $motorbus->readMotor(5,
+                                   $Device::Dynamixel::addresses{Present_Position_L}, 2);
+ my @params = @{$status->{params}};
+ my $position = $params[1]*255 + $params[0];
 
 =head1 DESCRIPTION
 
@@ -38,35 +42,113 @@ Dynamixel AX-12 motors have been tested to work with this module, but the others
 should work also.
 
 A daisy-chained series string of motors is connected to the host via a simple
-serial connection. Each motor in the series has an 8-bit address. This address
-is present in every command to address specific motors. The current
-implementation of this module uses an object per motor, NOT an object per string
-of motors. This may change in the future.
+serial connection. Each motor in the series has an 8-bit ID. This ID is present
+in every command to address specific motors. One Device::Dynamixel object should
+be created for a single string of motors connected to one motor port.
 
 These motors communicate using a particular protocol, which is implemented by
-this module. Commands are sent to the motor. A STATUS reply is sent back after
+this module. Commands are sent to the motor. A status reply is sent back after
 each command. This module handles construction and parsing of Dynamixel packets,
 as well as the sending and receiving data when needed.
 
-=head2 VARIABLES
+=head2 EXPORTED VARIABLES
 
-The dynamixel broadcast address is accessible as
-$Device::Dynamixel::BROADCAST_ADDR. The addresses of Dynamixel control registers
-are available as %Device::Dynamixel::addresses. This allows the application to
-send arbitrary commands to the motor. For example, to set the moving speed of
-the motor to 100%:
+To communicate with all motor at once, send commands to the broadcast ID:
 
- $dynamixel->writeMotor($Device::Dynamixel::addresses{Moving_Speed_L},
-                        [0xFF, 0x03]); # 0x3FF is the top speed for the
-                                       # Dynamixel AX-12
+ $Device::Dynamixel::BROADCAST_ID
+
+All the motor control addresses described in the Dynamixel docs are defined in this module,
+available as
+
+ $Device::Dynamixel::addresses{$value}
+
+Defined values are:
+
+ ModelNumber_L
+ ModelNumber_H
+ Version_of_Firmware
+ ID
+ Baud_Rate
+ Return_Delay_Time
+ CW_Angle_Limit_L
+ CW_Angle_Limit_H
+ CCW_Angle_Limit_L
+ CCW_Angle_Limit_H
+ Highest_Limit_Temperature
+ Lowest_Limit_Voltage
+ Highest_Limit_Voltage
+ Max_Torque_L
+ Max_Torque_H
+ Status_Return_Level
+ Alarm_LED
+ Alarm_Shutdown
+ Down_Calibration_L
+ Down_Calibration_H
+ Up_Calibration_L
+ Up_Calibration_H
+ Torque_Enable
+ LED
+ CW_Compliance_Margin
+ CCW_Compliance_Margin
+ CW_Compliance_Slope
+ CCW_Compliance_Slope
+ Goal_Position_L
+ Goal_Position_H
+ Moving_Speed_L
+ Moving_Speed_H
+ Torque_Limit_L
+ Torque_Limit_H
+ Present_Position_L
+ Present_Position_H
+ Present_Speed_L
+ Present_Speed_H
+ Present_Load_L
+ Present_Load_H
+ Present_Voltage
+ Present_Temperature
+ Registered_Instruction
+ Reserved
+ Moving
+ Lock
+ Punch_L
+ Punch_H
+
+To change the baud rate of the motor, the B<Baud_Rate> address must be written
+with a value
+
+ $Device::Dynamixel::baudrateValues{$baud}
+
+The available baud rates are
+ 1000000
+ 500000
+ 400000
+ 250000
+ 200000
+ 115200
+ 57600
+ 19200
+ 9600
+
+Note that the baud rate generally is cached from the last time the motor was
+used, defaulting to 1Mbaud at the start
+
+=head2 STATUS RETURN
+
+Most of the functions return a status hash that describes the status of the motors and/or returns
+queried data. This hash is defined as
+
+ { from   => $motorID,
+   error  => $error,
+   params => \@parameters }
+
+If no valid reply was received, undef is returned. Look at the Dynamixel hardware documentation for
+the exact meaning of each hash element.
 
 =cut
 
 
-
-
 # Constants defined in the dynamixel docs
-const our $BROADCAST_ADDR => 0xFE;
+const our $BROADCAST_ID => 0xFE;
 const my %instructions =>
   (PING       => 0x01,
    READ_DATA  => 0x02,
@@ -126,7 +208,7 @@ const our %addresses =>
    Punch_L                   => 48,
    Punch_H                   => 49);
 
-const my %baudrateValues =>
+const our %baudrateValues =>
   (1000000 => 0x01,
    500000  => 0x03,
    400000  => 0x04,
@@ -146,7 +228,7 @@ const my $motorRange_deg    => 300;
 
 =head1 CONSTRUCTOR
 
-=head2 new(PIPE, MOTORADDRESS)
+=head2 new( PIPE )
 
 Creates a new object to talk to a Dynamixel motor. The file handle has to be opened and set-up
 prior to constructing the object.
@@ -154,10 +236,9 @@ prior to constructing the object.
 =cut
 sub new
 {
-  my ($classname, $pipe, $motoraddr) = @_;
+  my ($classname, $pipe) = @_;
 
-  my $this = {pipe => $pipe,
-              addr => $motoraddr};
+  my $this = {pipe => $pipe};
   bless($this, $classname);
 
   return $this;
@@ -166,89 +247,66 @@ sub new
 # Constructs a binary dynamixel packet with a given command
 sub _makeInstructionPacket
 {
-  my ($addr, $instruction, $parameters) = @_;
-  my $body = pack( 'C3C' . @$parameters,
-                   $addr, 2 + @$parameters, $instruction,
+  my ($motorID, $instruction, $parameters) = @_;
+  my $body = pack( 'C3C' . scalar @$parameters,
+                   $motorID, 2 + @$parameters, $instruction,
                    @$parameters );
 
   my $checksum = ( ~sum(unpack('C*', $body)) & 0xFF );
   return pack('CC', 0xFF, 0xFF) . $body . chr $checksum;
 }
 
-# parses a given binary string as a dynamixel status packet
-sub _parseStatusPacket
-{
-  my $str = shift;
-
-  my ($key) = unpack('n', substr($str, 0, 2, '')) or return;
-
-  return if($key != 0xFFFF);
-
-  my ($addr, $length, $error) = unpack('C3', substr($str, 0, 3, '')) or return;
-  my $numParameters = $length - 2;
-  return if($numParameters < 0);
-
-  my @parameters = ();
-  my $sumParameters = 0;
-  if($numParameters)
-  {
-    @parameters = unpack("C$numParameters", substr($str, 0, $numParameters, '')) or return;
-    $sumParameters = sum(@parameters);
-  }
-  my $checksum = unpack('C1', substr($str, 0, 1, '')) // return;
-  my $checksumShouldbe = ~($addr + $length + $error + $sumParameters) & 0xFF;
-  return if($checksum != $checksumShouldbe);
-
-  return {from   => $addr,
-          error  => $error,
-          params => \@parameters};
-}
-
 =head1 METHODS
 
-=head2 pingMotor( )
+=head2 pingMotor( motorID )
 
-Sends a ping. STATUS reply is returned
+Sends a ping. Status reply is returned
 
 =cut
 
 sub pingMotor
 {
-  my $this = shift;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{PING}, []);
-  return pullMotorReply($pipe);
+  my $this      = shift;
+  my ($motorID) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{PING}, []);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 writeMotor(startingAddress, data)
+=head2 writeMotor( motorID, startingAddress, data )
 
-Sends a command to the motor. STATUS reply is returned.
+Sends a command to the motor. Status reply is returned.
 
 =cut
 
 sub writeMotor
 {
-  my ($this, $where, $what) = @_;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{WRITE_DATA}, [$where, @$what]);
-  return pullMotorReply($pipe);
+  my $this                     = shift;
+  my ($motorID, $where, $what) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{WRITE_DATA}, [$where, @$what]);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 readMotor(startingAddress, howManyBytes)
+=head2 readMotor( motorID, startingAddress, howManyBytes )
 
-Reads data from the motor. STATUS reply is returned.
+Reads data from the motor. Status reply is returned.
 
 =cut
 
 sub readMotor
 {
-  my ($this, $where, $howmany) = @_;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{READ_DATA}, [$where, $howmany]);
-  return pullMotorReply($pipe);
+  my $this                        = shift;
+  my ($motorID, $where, $howmany) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{READ_DATA}, [$where, $howmany]);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 writeMotor_queue(startingAddress, data)
+=head2 writeMotor_queue( motorID, startingAddress, data )
 
 Queues a particular command to the motor and returns the received reply. Does
 not actually execute the command until triggered with triggerMotorQueue( )
@@ -257,52 +315,58 @@ not actually execute the command until triggered with triggerMotorQueue( )
 
 sub writeMotor_queue
 {
-  my ($this, $where, $what) = @_;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{REG_WRITE}, [$where, @$what]);
-  return pullMotorReply($pipe);
+  my $this                     = shift;
+  my ($motorID, $where, $what) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{REG_WRITE}, [$where, @$what]);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 triggerMotorQueue( )
+=head2 triggerMotorQueue( motorID )
 
-Sends a trigger for the queued commands. STATUS reply is returned.
+Sends a trigger for the queued commands. Status reply is returned.
 
 =cut
 
 sub triggerMotorQueue
 {
-  my $this = shift;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{ACTION}, []);
-  return pullMotorReply($pipe);
+  my $this      = shift;
+  my ($motorID) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{ACTION}, []);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 resetMotor( )
+=head2 resetMotor( motorID )
 
-Sends a motor reset. STATUS reply is returned.
+Sends a motor reset. Status reply is returned.
 
 =cut
 
 sub resetMotor
 {
-  my $this = shift;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
-  print $pipe _makeInstructionPacket($addr, $instructions{RESET}, []);
-  return pullMotorReply($pipe);
+  my $this      = shift;
+  my ($motorID) = @_;
+
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($motorID, $instructions{RESET}, []);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 syncWriteMotor(startingAddress, data)
+=head2 syncWriteMotor( motorID, startingAddress, data )
 
-Sends a synced-write command to the motor. STATUS reply is returned.
+Sends a synced-write command to the motor. Status reply is returned.
 
 =cut
 
 sub syncWriteMotor
 {
-  my ($this, $writes, $where) = @_;
-  my ($pipe, $addr) = @{$this}{qw(pipe addr)};
+  my $this                       = shift;
+  my ($motorID, $writes, $where) = @_;
 
-  my @parms = map { ($_->{addr}, @{$_->{what}}) } @$writes;
+  my @parms = map { ($_->{motorID}, @{$_->{what}}) } @$writes;
   my $lenchunk = scalar @{$writes->[0]{what}};
   @parms = ($where, $lenchunk, @parms);
 
@@ -311,18 +375,12 @@ sub syncWriteMotor
     die "syncWriteMotor: size mismatch!";
   }
 
-  print $pipe _makeInstructionPacket($BROADCAST_ADDR, $instructions{SYNC_WRITE}, \@parms);
-  return pullMotorReply($pipe);
+  my $pipe = $this->{pipe};
+  print $pipe _makeInstructionPacket($BROADCAST_ID, $instructions{SYNC_WRITE}, \@parms);
+  return _pullMotorReply($this->{pipe});
 }
 
-=head2 pullMotorReply( )
-
-Performs a blocking read on the input pipe, and returns a parsed packet when
-it is received.
-
-=cut
-
-sub pullMotorReply
+sub _pullMotorReply
 {
   my $pipe = shift;
 
@@ -342,10 +400,40 @@ sub pullMotorReply
   }
 
   return _parseStatusPacket($packet);
+
+
+  # parses a given binary string as a dynamixel status packet
+  sub _parseStatusPacket
+  {
+    my $str = shift;
+
+    my ($key) = unpack('n', substr($str, 0, 2, '')) or return;
+
+    return if($key != 0xFFFF);
+
+    my ($motorID, $length, $error) = unpack('C3', substr($str, 0, 3, '')) or return;
+    my $numParameters = $length - 2;
+    return if($numParameters < 0);
+
+    my @parameters = ();
+    my $sumParameters = 0;
+    if ($numParameters)
+    {
+      @parameters = unpack("C$numParameters", substr($str, 0, $numParameters, '')) or return;
+      $sumParameters = sum(@parameters);
+    }
+    my $checksum = unpack('C1', substr($str, 0, 1, '')) // return;
+    my $checksumShouldbe = ~($motorID + $length + $error + $sumParameters) & 0xFF;
+    return if($checksum != $checksumShouldbe);
+
+    return {from   => $motorID,
+            error  => $error,
+            params => \@parameters};
+  }
 }
 
 
-=head2 moveMotorTo_deg(position_degrees)
+=head2 moveMotorTo_deg( motorID, position_degrees )
 
 Convenience function that uses the lower-level routines to move a motor to a
 particular position
@@ -354,13 +442,13 @@ particular position
 
 sub moveMotorTo_deg
 {
-  my $this         = shift;
-  my $position_deg = shift;
+  my $this                     = shift;
+  my ($motorID, $position_deg) = @_;
 
   my $position = int( 0.5 + ($position_deg * $motorRange_coords/$motorRange_deg + 0x1ff) );
   $position    = 0                    if $position <  0;
   $position    = $motorRange_coords-1 if $position >= $motorRange_coords;
-  return $this->writeMotor($addresses{Goal_Position_L}, [unpack('C2', pack('v', $position))] );
+  return $this->writeMotor($motorID, $addresses{Goal_Position_L}, [unpack('C2', pack('v', $position))] );
 }
 
 1;
@@ -371,16 +459,9 @@ __END__
 
 =head1 BUGS
 
-There are several aspects of this module that are unideal and may change in the
-future. The current implementation ties an instance of a Device::Dynamixel
-object to a particular motor, NOT to a string of motors. This means that talking
-to multiple motors requires multiple instances. This imposes a requirement that
-only one motor can be controlled at any given time. Further, multi-motor
-commands like syncWriteMotor( ) become essentially useless.
-
-Another major issue is the baud rate of the serial communication. The motors
-default to 1M baud. This is unsupported by the stock POSIX module in perl5, so
-the serial port must be configured external to this module.
+An issue is the baud rate of the serial communication. The motors default to 1M
+baud. This is unsupported by the stock POSIX module in perl5, so the serial port
+must be configured externally, prior to using to this module.
 
 =head1 REPOSITORY
 
